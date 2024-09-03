08e59215d0f96f4e60deb8e8e117e8866b33fbe8
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-09-03 13:03:49 +0200

    Update predict_price.py

--- scripts/predict_price.py
+++ scripts/predict_price.py
@@ -3,7 +3,6 @@ def predict_price(mileage, theta0, theta1):
 
 
 def main():
-    # Charger les valeurs de theta0 et theta1
     try:
         with open('../theta_values.txt', 'r') as file:
             theta0, theta1 = map(float, file.read().split())
@@ -11,10 +10,8 @@ def main():
         print("Veuillez d'abord entraîner le modèle pour générer theta_values.txt.")
         return
 
-    # Demander le kilométrage à l'utilisateur
     mileage = float(input("Entrez le kilométrage de la voiture: "))
 
-    # Prédire le prix
     price = predict_price(mileage, theta0, theta1)
 
     print(f"Le prix estimé pour un kilométrage de {mileage} est {price:.2f}.")
